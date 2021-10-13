9f4b00d9d4adcb656a2c7d3003795f59b965052c
Christian Tielking <tielking5@gmail.com>
2021-10-13 13:50:06 -0400

    added semantics

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -31,6 +31,10 @@ module.exports = {
     Mutation: {
         async createPost(_, { body }, context) {
             const user = checkAuth(context);
+
+            if (args.body.trim() === '') {
+                throw new Error('Post must not be empty')
+            }
             
             const newPost = new Post({
                 body,
